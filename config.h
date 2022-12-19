9713404f6ae82615f77d0dfac73436a56e96acb5
whou <willianvinagre11@gmail.com>
2022-12-19 14:40:36 -0300

    add emojis

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 5;        /* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "bitstreamverasansmono:size=12" };
+static const char *fonts[]          = { "bitstreamverasansmono:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "bitstreamverasansmono:size=12";
 //background color
 static const char col_gray1[]       = "#222222";
